2ab6ab8537450018aa1385864ef0c81fe337b293
Jeff Hutchins <zumpiez@gmail.com>
2021-12-21 14:06:26 -0500

    day 4

--- 2021/4.ts
+++ 2021/4.ts
@@ -1,6 +1,21 @@
 async function main() {
-  const {hopper, boards} = new BingoConfig(await Deno.readTextFile("input/4.txt"));
-  
+  const {hopper, boards} = new BingoConfig(await Deno.readTextFile("./input/4.txt"));
+
+  let winningBoards: BingoBoard[] = [];
+
+  for (const ball of hopper) {
+    for (const board of boards) {
+      if (board.score == false) {
+        board.markValue(ball);
+        if (board.score != false) {
+          winningBoards.push(board);
+        }
+      }
+    }
+  }
+
+  console.log(`First winner (part 1): ${winningBoards[0].score}
+  Final winner (part 2): ${winningBoards.at(-1)!.score}`);
 }
 
 // impl
@@ -17,7 +32,7 @@ class BingoConfig {
     
     // Remaining lines are parsed as bingo grids
     for(let i = 1; i < lines.length; ++i) {
-      if (lines[i] == "") {
+      if (lines[i] == "\r") {
         this.boards.push(new BingoBoard());
       } else {
         // Parse this line into the current board as a row of whitespace-delimited numbers
@@ -32,18 +47,79 @@ class BingoConfig {
   }
 }
 
-class BingoBoard {
-  values: number[] = [];
-  
-  insertValue(value: number): void {
-    this.values.push(value);
+class BoardSpace {
+  value: number;
+  marked: boolean;
+
+  constructor(value: number) {
+    this.value = value;
+    this.marked = false;
   }
-  
+}
+
+class BingoBoard {
+  spaces: BoardSpace[] = [];
+  score: number|false = false;
+
+  insertValue(value: number): void {
+    this.spaces.push(new BoardSpace(value));
+  }
+
   markValue(value: number): void {
-    const found = this.values.findIndex(v => v == value)
+    if (this.score != false) return; //don't mark boards that already have a bingo
+
+    const found = this.spaces.findIndex(s => s.value == value)
     if (found > -1) {
-      
+      this.spaces[found].marked = true;
     }
+
+    if (this.checkWin()) {
+      this.score = this.computeScore(value);
+    }
+  }
+
+  getRow(index: number): BoardSpace[] {
+    const rowStart = index * 5;
+    const rowEnd = rowStart + 5;
+
+    return this.spaces.slice(rowStart, rowEnd);
+  }
+
+  getColumn(index: number): BoardSpace[] {
+    const colStart = index;
+    const stride = 5;
+
+    let result: BoardSpace[] = [];
+
+    for(let i = 0; i < 5; ++i) {
+      result.push(this.spaces[colStart + stride*i])
+    }
+
+    return result;
+  }
+
+  checkWin(): boolean {
+    for(let i = 0; i < 5; ++i) {
+      const row = this.getRow(i);
+      if (row.every((space) => space.marked == true)) return true;
+
+      const col = this.getColumn(i);
+      if (col.every((space) => space.marked == true)) return true;
+    }
+
+    return false;
+  }
+
+  computeScore(finalBall: number): number {
+    // Sum all of the UNMARKED numbers on the board
+    let unmarkedTotal = 0;
+    for (const space of this.spaces) {
+      if (!space.marked) {
+        unmarkedTotal += space.value;
+      }
+    }
+
+    return unmarkedTotal * finalBall;
   }
 }
 
